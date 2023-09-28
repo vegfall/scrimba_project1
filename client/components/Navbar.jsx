f91f2d690a6fdfb231dec833c4ed13c763783428
Vegard Falldalen <vegfall@gmail.com>
2023-09-28 10:20:14 +0200

    Made beautiful navbar

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import react_logo from "./../images/react-icon-small.png"
 
 export default function Navbar() {
     return (
-        <h1>Navbar goes here</h1>
+        <nav className="navbar_nav">
+            <img src={react_logo} className="navbar_react_logo" alt="react logo"/>
+            <h3 className="navbar_h3">ReactFacts</h3>
+            <h4 className="navbar_h4">React Course - Project 1</h4>
+        </nav>
     )
 }
